e395858eda90cab66f167c9a73dfd927e552d224
adeepabandara <adeepabandara@gmail.com>
2022-01-20 03:00:53 +0530

    Initial Upload

---
+++ form.php
@@ -0,0 +1,292 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1">
+    <title>Main</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/lykmapipo/themify-icons@0.1.2/css/themify-icons.css">
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+<input type="checkbox" id="sidebar-toggle">
+    <div class="sidebar">
+        <div class="sidebar-header">
+            <h3>
+                <span>MOH Sri Lanka</span>
+            </h3>
+
+            <label for="sidebar-toggle" class="ti-menu-alt"></label>
+        </div>
+
+        <div class="sidebar-user">
+            <span>
+            <img src="img/logo.png" alt="">
+        </span>
+
+            <h3>MOH Kandy</h3>
+
+        </div>
+
+        <div class="sidebar-menu">
+
+
+            <ul>
+                <li>
+                    <a href="index.html">
+                        <span class=""></span>
+                        <span>Home</span>
+                    </a>
+                </li>
+             
+                <li>
+                    <a href="programmes.html">
+                        <span class=""></span>
+                        <span>Programmes</span>
+                    </a>
+                </li>
+             
+                <li>
+                    <a href="employee.php">
+                        <span class=""></span>
+                        <span>Employee Database</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="">
+                        <span class=""></span>
+                        <span>Account</span>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+
+    <div class="main-content">
+
+        <header>
+
+        </header>
+
+        <main>
+            <div class="summary">
+
+                <div class="cards1">
+
+                    <div class="card-single">
+                        <div class="card-body">
+                            
+
+                            <div class="card-head">
+                                <span>Division - 002</span>
+                                <h1>St.Marys College</h1>
+                                <h3>21/02/2022</h3>
+                            </div>
+                        </div>
+                    </div>
+
+
+                    <div class="card-single">
+                        <div class="card-body">
+                           
+
+                            <div class="card-head">
+                                <span>Vaccinies Brand</span>
+                                <h1>Pfizer</h1>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="card-single">
+                        <div class="card-body">
+                           
+
+                            <div class="card-head">
+                                <span>Vaccinies Available</span>
+                                <h1>156</h1>
+                            </div>
+                        </div>
+
+                    </div>
+
+                </div>
+
+
+                <section class="recent">
+                    <div class="activity-grid1">
+                        <div class="activity-card">
+                            <div class="wrapper">
+                                <div class="form">
+
+                                    <div class="inputfield">
+
+
+                                        <div class="text-center1">
+                                            <input type="text" class="input" placeholder="Search NIC">
+
+                                            <button type="submit">Search Record</button>
+
+
+
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+
+                    <div class="activity-grid1">
+                        <div class="activity-card">
+                            <div class="wrapper">
+                                <div class="title">
+
+                                    <div class="form">
+                                        <div class="title">
+                                            Vaccinated Pateint Details
+                                        </div>
+
+                                        <div class="form">
+                                            <div class="inputfield">
+                                                <label>First Name</label>
+                                                <input type="text" class="input">
+                                            </div>
+                                            <div class="inputfield">
+                                                <label>Last Name</label>
+                                                <input type="text" class="input">
+                                            </div>
+
+                                            <div class="inputfield">
+                                                <label>NIC</label>
+                                                <input type="text" class="input">
+                                            </div>
+                                            <div class="inputfield">
+                                                <label>Gender</label>
+                                                <div class="custom_select">
+                                                    <select>
+                                                        <option value="">Select</option>
+                                                        <option value="Male">Male</option>
+                                                        <option value="Female">Female</option>
+
+                                                    </select>
+                                                </div>
+                                            </div>
+
+                                            <div class="inputfield">
+                                                <label>Address</label>
+                                                <textarea class="textarea"></textarea>
+                                            </div>
+                                            <div class="inputfield">
+                                                <label>Division</label>
+                                                <input type="text" class="input">
+                                            </div>
+
+                                            <div class="inputfield">
+
+                                                <label>1<sup>st</sup>Vaccine Date</label>
+                                                <input type="date" id="" name="">
+                                            </div>
+
+                                            <div class="inputfield">
+                                                <label>1<sup>st</sup> Vaccine Brand</label>
+                                                <div class="custom_select">
+                                                    <select>
+                                                        <option value="">Select</option>
+                                                        <option value="Sinopharm">Sinopharm</option>
+                                                        <option value="Astrezenica">Astrezenica</option>
+                                                        <option value="Pifzer">Pifzer</option>
+                                                        <option value="Moderna">Moderna</option>
+                                                        <option value="Sputnik">Sputnik</option>
+                                                    </select>
+                                                </div>
+                                            </div>
+                                            <div class="inputfield">
+
+                                                <label>2<sup>nd</sup>Vaccine Date</label>
+                                                <input type="date" id="" name="">
+                                            </div>
+
+                                            <div class="inputfield">
+                                                <label>2<sup>nd</sup> Vaccine Brand</label>
+                                                <div class="custom_select">
+                                                    <select>
+                                                        <option value="">Select</option>
+                                                        <option value="Sinopharm">Sinopharm</option>
+                                                        <option value="Astrezenica">Astrezenica</option>
+                                                        <option value="Pifzer">Pifzer</option>
+                                                        <option value="Moderna">Moderna</option>
+                                                        <option value="Sputnik">Sputnik</option>
+                                                    </select>
+                                                </div>
+                                            </div>
+
+                                            <div class="inputfield">
+
+                                                <label>3<sup>rd</sup>Vaccine Date</label>
+                                                <input type="date" id="" name="">
+                                            </div>
+
+                                            <div class="inputfield">
+                                                <label>3<sup>rd</sup> Vaccine Brand</label>
+                                                <div class="custom_select">
+                                                    <select>
+                                                        <option value="">Select</option>
+                                                        <option value="Sinopharm">Sinopharm</option>
+                                                        <option value="Astrezenica">Astrezenica</option>
+                                                        <option value="Pifzer">Pifzer</option>
+                                                        <option value="Moderna">Moderna</option>
+                                                        <option value="Sputnik">Sputnik</option>
+                                                    </select>
+                                                </div>
+                                            </div><br>
+
+                                            
+                                            
+
+                                        
+
+                                            <div class="text-center1">
+
+                                                <button type="submit">Update / Register</button>
+                                            </div>
+
+
+                                        </div>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+            </div>
+
+            <div class="bday-card">
+                <div class="bday-flex">
+                    <div class="emer-img"></div>
+                    <div class="bday-info">
+                        <h5>Emergeny Requirements </h5>
+                        <small>Ministry of Health Head Office</small>
+                    </div>
+                </div>
+
+                <div class="text-center">
+                    <button>
+                        Contact Immediately
+                    </button>
+                </div>
+            </div>
+
+    </div>
+    </div>
+    </section>
+
+    </main>
+
+    </div>
+
+</body>
+
+</html>
